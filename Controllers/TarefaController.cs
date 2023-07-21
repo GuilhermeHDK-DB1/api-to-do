b7bb36d6fd9a07706ecb20697d466788f7245b7a
GuilhermeHDK-DB1 <guilherme.shibukawa@db1.com.br>
2023-07-21 16:56:57 -0300

    first commit

---
+++ Controllers/TarefaController.cs
@@ -0,0 +1,45 @@
+﻿using api_to_do.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace api_to_do.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TarefaController : ControllerBase
+{
+    private static List<Tarefa> tarefas = new List<Tarefa>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult CreateTask([FromBody] Tarefa tarefa)
+    {
+        tarefa.Id = id++;
+        tarefas.Add(tarefa);
+        return CreatedAtAction(nameof(GetTarefaById), 
+            new { id = tarefa.Id }, 
+            tarefa);
+    }
+
+    [HttpGet]
+    public IEnumerable<Tarefa> GetTarefaPage([FromQuery] int skip=0, [FromQuery] int take=50)
+    {
+        return tarefas.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetTarefaById(int id) { 
+        var tarefa = tarefas.FirstOrDefault(tarefa =>  tarefa.Id == id);
+        if (tarefa == null) return NotFound();
+        return Ok(tarefa);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteTarefa(int id) {
+        var tarefa = tarefas.FirstOrDefault(tarefa => tarefa.Id == id);
+        if (tarefa == null) return NotFound();
+        tarefas.Remove(tarefa);
+        return NoContent();
+
+    }
+}
---
+++ Models/Tarefa.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_to_do.Models;
+
+public class Tarefa
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O campo title é obrigatório")]
+    public string Title { get ; set; }
+    [Required(ErrorMessage = "O campo description é obrigatório")]
+    public string Description { get; set; }
+    [Required(ErrorMessage = "O campo state é obrigatório")]
+    public string State { get; set; }
+
+}
