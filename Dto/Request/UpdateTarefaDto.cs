c3fcffceb5b840c096e4f329a89edd5ec3c249d6
GuilhermeHDK-DB1 <guilherme.shibukawa@db1.com.br>
2023-07-24 09:05:45 -0300

    atualiza tarefa

--- Dto/Request/UpdateTarefaDto.cs
+++ Dto/Request/UpdateTarefaDto.cs
@@ -4,10 +4,7 @@ namespace api_to_do.Dto.Request;
 
 public class UpdateTarefaDto
 {
-    [Required(ErrorMessage = "O campo title é obrigatório")]
-    public string Title { get; set; }
-    [Required(ErrorMessage = "O campo description é obrigatório")]
-    public string Description { get; set; }
-    [Required(ErrorMessage = "O campo state é obrigatório")]
-    public string State { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? State { get; set; }
 }
